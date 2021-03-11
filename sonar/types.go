13fc6aef3b18b73aa5008ab4e39f3c0ee82b8eed
Ahmed Alsabag <ahmed.alsabag@gmail.com>
2021-03-11 11:00:08 -0600

    adding more types for additional information from sonar

--- sonar/types.go
+++ sonar/types.go
@@ -56,3 +56,119 @@ type Condition struct {
 	Operator       string `json:"operator"`
 	Status         string `json:"status"`
 }
+
+// Overall Measures
+type OverallMeasures struct {
+	Component struct {
+		ID        string `json:"id"`
+		Key       string `json:"key"`
+		Name      string `json:"name"`
+		Qualifier string `json:"qualifier"`
+		Measures  []struct {
+			Metric    string `json:"metric"`
+			Value     string `json:"value,omitempty"`
+			BestValue bool   `json:"bestValue,omitempty"`
+			Periods   []struct {
+				Index     int    `json:"index"`
+				Value     string `json:"value"`
+				BestValue bool   `json:"bestValue"`
+			} `json:"periods,omitempty"`
+			Period struct {
+				Index     int    `json:"index"`
+				Value     string `json:"value"`
+				BestValue bool   `json:"bestValue"`
+			} `json:"period,omitempty"`
+		} `json:"measures"`
+	} `json:"component"`
+	Metrics []struct {
+		Key                   string `json:"key"`
+		Name                  string `json:"name"`
+		Description           string `json:"description"`
+		Domain                string `json:"domain"`
+		Type                  string `json:"type"`
+		HigherValuesAreBetter bool   `json:"higherValuesAreBetter,omitempty"`
+		Qualitative           bool   `json:"qualitative"`
+		Hidden                bool   `json:"hidden"`
+		Custom                bool   `json:"custom"`
+		BestValue             string `json:"bestValue,omitempty"`
+		DecimalScale          int    `json:"decimalScale,omitempty"`
+		WorstValue            string `json:"worstValue,omitempty"`
+	} `json:"metrics"`
+	Periods []struct {
+		Index int    `json:"index"`
+		Mode  string `json:"mode"`
+		Date  string `json:"date"`
+	} `json:"periods"`
+}
+
+// Individual Issues
+type Issues struct {
+	Total  int `json:"total"`
+	P      int `json:"p"`
+	Ps     int `json:"ps"`
+	Paging struct {
+		PageIndex int `json:"pageIndex"`
+		PageSize  int `json:"pageSize"`
+		Total     int `json:"total"`
+	} `json:"paging"`
+	EffortTotal int `json:"effortTotal"`
+	DebtTotal   int `json:"debtTotal"`
+	Issues      []struct {
+		Key       string `json:"key"`
+		Rule      string `json:"rule"`
+		Severity  string `json:"severity"`
+		Component string `json:"component"`
+		Project   string `json:"project"`
+		Line      int    `json:"line"`
+		Hash      string `json:"hash"`
+		TextRange struct {
+			StartLine   int `json:"startLine"`
+			EndLine     int `json:"endLine"`
+			StartOffset int `json:"startOffset"`
+			EndOffset   int `json:"endOffset"`
+		} `json:"textRange"`
+		Flows        []interface{} `json:"flows"`
+		Status       string        `json:"status"`
+		Message      string        `json:"message"`
+		Effort       string        `json:"effort"`
+		Debt         string        `json:"debt"`
+		Tags         []string      `json:"tags"`
+		Transitions  []interface{} `json:"transitions"`
+		Actions      []interface{} `json:"actions"`
+		Comments     []interface{} `json:"comments"`
+		CreationDate string        `json:"creationDate"`
+		UpdateDate   string        `json:"updateDate"`
+		Type         string        `json:"type"`
+		Organization string        `json:"organization"`
+		Scope        string        `json:"scope"`
+	} `json:"issues"`
+	Components []struct {
+		Organization string `json:"organization"`
+		Key          string `json:"key"`
+		UUID         string `json:"uuid"`
+		Enabled      bool   `json:"enabled"`
+		Qualifier    string `json:"qualifier"`
+		Name         string `json:"name"`
+		LongName     string `json:"longName"`
+		Path         string `json:"path,omitempty"`
+	} `json:"components"`
+	Rules []struct {
+		Key      string `json:"key"`
+		Name     string `json:"name"`
+		Lang     string `json:"lang"`
+		Status   string `json:"status"`
+		LangName string `json:"langName"`
+	} `json:"rules"`
+	Users     []interface{} `json:"users"`
+	Languages []struct {
+		Key  string `json:"key"`
+		Name string `json:"name"`
+	} `json:"languages"`
+	Facets []struct {
+		Property string `json:"property"`
+		Values   []struct {
+			Val   string `json:"val"`
+			Count int    `json:"count"`
+		} `json:"values"`
+	} `json:"facets"`
+}
